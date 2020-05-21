0fc5047cb00bb2cb80c24141dc8e03a898d234eb
shaynavins <51338693+shaynavins@users.noreply.github.com>
2020-05-21 09:25:36 -0400

    Update display.php

--- display.php
+++ display.php
@@ -1,8 +1,52 @@
 
+  <html>
+<head>
+<style>
+table {
+  font-family: fantasy; sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 5px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(odd) {
+  background-color:silver;
+}
+
+    body{
+            background-image:url("berry.jpg");
+        font-family: 'Indie Flower';font-size: 22px;
+            color: black;
+        }
+
+.btn {
+  background-color:aquamarine;
+  border: none;
+  color: black;
+  padding: 12px 16px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+  background-color: aqua;
+}
+</style>
+</head>
+    
+<body>
+    <span class="error"> <?php if(isset($error['name']))
+echo $error['name'];?></span>
     <?php
-include 'find.php';
+//include 'find.php';
 $num = $_GET['id'];
-echo $num;
+//echo $num;
 $connect = mysqli_connect("localhost","root","gue55me", "dish");
 
 $find = "SELECT pic, text, ID FROM images WHERE ID = '$num'";
@@ -24,5 +68,18 @@ if ($row1 > 0) {
 }else {
   echo "0 results";
 }
-$connect->close();
+  $result = mysqli_query($connect, "SELECT * FROM images WHERE ID = '$num'");
+    while ($row = mysqli_fetch_array($result)) {
+      echo "<div id='img_div'>";
+      	echo "<img src='images/".$row['picture']."' >";
+      echo "</div>";
+    }
+    echo "<form method='post' action='form1.html'>";
+    echo "<button type='submit' class='btn'>Take me back!</button>";
+    echo "</form>";
 ?>
+        <!--<form method="post" action="form1.html">
+    <button type="submit" class="btn">Take me back!</button>
+        </form>
+    </html>-->
+
